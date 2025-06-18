f711b4e5c5fdd8b965dc1fdbfedb3cfaf7483a49
globalop <Amitkmishra0641@gmail.com>
2025-06-18 10:28:44 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+from mlproject.components.data_ingestion import DataIngestion
+from mlproject.components.data_ingestion import DataIngestionConfig
 
 from mlproject.logger import logging
 from mlproject.exception import CustomException
@@ -12,7 +14,10 @@ if __name__ =="__main__":
     logging.info("the execution has started")
 
     try:
-        a=1/0
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
